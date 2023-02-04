0a9e16394d4bd8cec4ff331d54a58dc1ad1e1857
Yemi <tywonderkid@yahoo.com>
2023-02-04 04:50:52 -0600

    Frontend Script

--- frontend.sh
+++ frontend.sh
@@ -6,7 +6,7 @@ systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip -o /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
