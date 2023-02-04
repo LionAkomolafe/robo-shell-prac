9300888fc0da9056386b580c8b6e906669123a0f
Yemi <tywonderkid@yahoo.com>
2023-02-04 04:39:55 -0600

    User Script

---
+++ user.sh
@@ -0,0 +1,17 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+useradd roboshop
+cd /home/roboshop
+unzip /tmp/user.zip
+mv user-main user
+cd user
+npm install
+
+sed -i -e 's/REDIS_ENDPOINT/redis.kingyamza.online/' -e 's/MONGO_ENDPOINT/mongo.kingyamza.online/' systemd.service
+cd ~
+mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
